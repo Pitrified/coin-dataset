6c4964a22ff40520980815dbf9fcf16204c9bd4e
Pitrified <nobilipietro@gmail.com>
2019-07-20 13:59:24 +0200

    Stop fighting windmills
    Wider min/max radius constraint, filter later. Using the number of coins
    in an image refines even more the method.

--- extract_coins.py
+++ extract_coins.py
@@ -4,6 +4,7 @@ import numpy as np
 
 from os import listdir
 from os import makedirs
+from os import sep
 from os.path import abspath
 from os.path import dirname
 from os.path import splitext
@@ -58,6 +59,13 @@ def crop_circle(img_orig, x, y, radius, pad, show_crop=False):
 
     # crop the circle
     piece = img_orig[left:right, top:bottom, :]
+    #  print(f"Piece shape {piece.shape}")
+
+    if piece.shape[0] % 2 != 0 or piece.shape[1] % 2 != 0:
+        print(f"Non-even shape {piece.shape}")
+
+    if piece.shape[0] != piece.shape[1]:
+        print(f"Non-square shape {piece.shape}")
 
     if show_crop:
         cv2.imshow("Piece", piece)
@@ -78,22 +86,43 @@ def find_coins_circles(image_name_full, hcParam, show_clahed=False):
 
     img_orig = cv2.GaussianBlur(img_orig, (5, 5), 5)
 
-    #  img_orig = cv2.GaussianBlur(img_orig, (3, 3), 6)
-    #  cv2.imshow("Original", img_orig)
+    #  cv2.imshow("Blurred", img_orig)
     #  cv2.waitKey(0)
 
-    #  circles = cv2.HoughCircles(image=img_orig, **hcParam)
-
     img_clahed = equalize_gray_clahe(img_orig)
-    scale = 800 / max(img_clahed.shape[0:2])
-    img_clahed_small = cv2.resize(
-        img_clahed, None, fx=scale, fy=scale, interpolation=cv2.INTER_AREA
-    )
-    if show_clahed:
-        cv2.imshow("Clahed", img_clahed_small)
-        #  cv2.waitKey(0)
 
     circles = cv2.HoughCircles(image=img_clahed, **hcParam)
+
+    if show_clahed:
+        img_orig = cv2.imread(image_name_full)
+        if not circles is None:
+            for x, y, r in circles[0, :, :]:
+                cv2.circle(img_orig, (x, y), r, thickness=5, color=(0, 255, 0))
+                descr = f"({int(x)}, {int(y)}): {int(r)}"
+                xt = int(x - r - 10)
+                yt = int(y - r - 40)
+                cv2.putText(
+                    img_orig,
+                    descr,
+                    (xt, yt),
+                    fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                    fontScale=2,
+                    color=(0, 0, 255),
+                    thickness=3,
+                )
+
+        scale = 1000 / max(img_orig.shape[0:2])
+        img_orig_small = cv2.resize(
+            img_orig, None, fx=scale, fy=scale, interpolation=cv2.INTER_AREA
+        )
+
+        cv2.imshow("Circled", img_orig_small)
+        key = cv2.waitKey(0)
+        #  print(f"{key}") # key == 13 ENTER; key == 32 SPACE;
+        if key == 98:  # b
+            print(f"\t\t\tBad circles extracted")
+        elif key == 97:  # a
+            print(f"\t\t\tBad circles extracted in previous one")
 
     #  if not circles is None:
     #  print(f"Circles found {circles.shape[1]}")
@@ -113,11 +142,16 @@ def do_extraction(raw_folder_full, save_folder_full, pad):
     Output images: same folders, in save_folder_full
     """
     show_crop = False
-    show_clahed = False
-    save_results = True
+    #  show_clahed = False
+    show_clahed = True
+    save_results = False
+    #  save_results = True
 
-    #  for label in ["10c"]:
-    for label in listdir(raw_folder_full):
+    raw_folder_tag = raw_folder_full.split(sep)[-1]
+    print(f"\nRAW folder tag: {raw_folder_tag}\n")
+
+    #  for label in listdir(raw_folder_full):
+    for label in ["2e"]:
         print(f"\nLABEL: {label}\n")
         full_label = join(raw_folder_full, label)
 
@@ -126,8 +160,8 @@ def do_extraction(raw_folder_full, save_folder_full, pad):
         if not isdir(output_labeled_folder_full):
             makedirs(output_labeled_folder_full)
 
-        #  for image_name in listdir(full_label)[:2]:
-        for image_name in listdir(full_label):
+        #  for image_name in listdir(full_label)[:4]:
+        for image_name in sorted(listdir(full_label)):
 
             #  print(f"Image name: {image_name}")
             image_name_full = join(full_label, image_name)
@@ -139,7 +173,9 @@ def do_extraction(raw_folder_full, save_folder_full, pad):
             print(f"Image name base full: {image_name_base_full}")
 
             # EXTRACT circles from image
-            circles = find_coins_circles(image_name_full, getHCpar(label), show_clahed)
+            circles = find_coins_circles(
+                image_name_full, getHCpar(label, raw_folder_tag), show_clahed
+            )
             print(f"\tCircles found {circles.shape[0]}")
 
             img_orig = cv2.imread(image_name_full)
@@ -151,118 +187,173 @@ def do_extraction(raw_folder_full, save_folder_full, pad):
                         out_name_full = f"{image_name_base_full}_{i}{image_name_ext}"
                         #  print(f"\tout_name_full {out_name_full}")
                         cv2.imwrite(out_name_full, img_piece)
+                else:
+                    print(f"\tBad circle {i}")
 
 
-def getHCpar(label):
+def getHCpar(label, raw_folder_tag):
     """Return a dict of params for HoughCircles
+
+    Different params for different photo sets
     """
-    if label in ["2e"]:
-        param = {
-            "method": cv2.HOUGH_GRADIENT,
-            "dp": 1.0,
-            "minDist": 140,
-            #  "minRadius": 170,
-            "minRadius": 170,
-            "maxRadius": 230,
-            #  "maxRadius": 320,
-            #  "param1": 140,
-            "param1": 135,
-            "param2": 60,
-        }
-    elif label in ["1e"]:
-        # 1e minDist 40 minRadius 165 maxRadius 190 hcParam1 70 hcParam2 25
-        param = {
-            "method": cv2.HOUGH_GRADIENT,
-            "dp": 1.0,
-            #  "minDist": 40,
-            "minDist": 200,
-            "minRadius": 165,
-            "maxRadius": 190,
-            "param1": 70,
-            "param2": 25,
-        }
-    elif label in ["1c"]:
-        # 1c minDist 150 minRadius 125 maxRadius 160 hcParam1 65 hcParam2 30
-        param = {
-            "method": cv2.HOUGH_GRADIENT,
-            "dp": 1.0,
-            "minDist": 150,
-            #  "minRadius": 125,
-            #  "maxRadius": 160,
-            "minRadius": 110,
-            "maxRadius": 140,
-            "param1": 65,
-            "param2": 30,
-        }
-    elif label in ["2c"]:
-        # 2c TODO
-        param = {
-            "method": cv2.HOUGH_GRADIENT,
-            "dp": 1.0,
-            #  "minDist": 150,
-            "minDist": 200,
-            #  "minRadius": 125,
-            "minRadius": 135,
-            "maxRadius": 160,
-            "param1": 65,
-            "param2": 30,
-        }
-    elif label in ["5c"]:
-        # 5c minDist 150 minRadius 150 maxRadius 170 hcParam1 60 hcParam2 25
-        param = {
-            "method": cv2.HOUGH_GRADIENT,
-            "dp": 1.0,
-            "minDist": 150,
-            "minRadius": 150,
-            "maxRadius": 170,
-            "param1": 60,
-            "param2": 25,
-        }
-    elif label in ["10c"]:
-        # 10c minDist 150 minRadius 140 maxRadius 160 hcParam1 100 hcParam2 41
-        param = {
-            "method": cv2.HOUGH_GRADIENT,
-            "dp": 1.0,
-            "minDist": 150,
-            "minRadius": 140,
-            "maxRadius": 160,
-            #  "param1": 100,
-            "param1": 70,
-            #  "param2": 40,
-            "param2": 30,
-        }
-    elif label in ["20c"]:
-        # 20c
-        param = {
-            "method": cv2.HOUGH_GRADIENT,
-            "dp": 1.0,
-            #  "minDist": 150,
-            "minDist": 200,
-            "minRadius": 150,
-            "maxRadius": 180,
-            "param1": 65,
-            "param2": 30,
-        }
-    elif label in ["50c"]:
-        # 50c minDist 150 minRadius 165 maxRadius 200 hcParam1 80 hcParam2 45
-        param = {
-            "method": cv2.HOUGH_GRADIENT,
-            "dp": 1.0,
-            "minDist": 150,
-            #  "minRadius": 165,
-            "minRadius": 150,
-            "maxRadius": 200,
-            "param1": 80,
-            "param2": 45,
-        }
+    if raw_folder_tag in ["raw4"]:
+        if label in ["2e"]:
+            param = {
+                "method": cv2.HOUGH_GRADIENT,
+                "dp": 1.0,
+                "minDist": 140,
+                "minRadius": 150,
+                "maxRadius": 230,
+                #  "param1": 140,
+                "param1": 135,
+                "param2": 60,
+            }
+    elif raw_folder_tag in ["raw3"]:
+        if label in ["1c"]:
+            # 1c minDist 150 minRadius 125 maxRadius 160 hcParam1 65 hcParam2 30
+            param = {
+                "method": cv2.HOUGH_GRADIENT,
+                "dp": 1.0,
+                "minDist": 150,
+                #  "minRadius": 125,
+                #  "maxRadius": 160,
+                "minRadius": 110,
+                "maxRadius": 140,
+                "param1": 65,
+                "param2": 30,
+            }
+        elif label in ["2c"]:
+            # 2c TODO
+            param = {
+                "method": cv2.HOUGH_GRADIENT,
+                "dp": 1.0,
+                #  "minDist": 150,
+                "minDist": 200,
+                #  "minRadius": 125,
+                "minRadius": 135,
+                "maxRadius": 160,
+                "param1": 65,
+                "param2": 30,
+            }
+        elif label in ["5c"]:
+            # 5c minDist 150 minRadius 150 maxRadius 170 hcParam1 60 hcParam2 25
+            param = {
+                "method": cv2.HOUGH_GRADIENT,
+                "dp": 1.0,
+                "minDist": 150,
+                "minRadius": 150,
+                #  "maxRadius": 170,
+                "maxRadius": 165,
+                "param1": 60,
+                "param2": 25,
+            }
+        elif label in ["20c"]:
+            # 20c
+            param = {
+                "method": cv2.HOUGH_GRADIENT,
+                "dp": 1.0,
+                #  "minDist": 150,
+                #  "minDist": 200,
+                "minDist": 250,
+                #  "minRadius": 150,
+                "minRadius": 155,
+                "maxRadius": 180,
+                "param1": 65,
+                "param2": 30,
+            }
+        elif label in ["50c"]:
+            # 50c minDist 150 minRadius 165 maxRadius 200 hcParam1 80 hcParam2 45
+            param = {
+                "method": cv2.HOUGH_GRADIENT,
+                "dp": 1.0,
+                "minDist": 150,
+                #  "minRadius": 165,
+                "minRadius": 150,
+                "maxRadius": 200,
+                "param1": 80,
+                "param2": 45,
+            }
+    elif raw_folder_tag in ["raw4"]:
+        if label in ["2e"]:
+            param = {
+                "method": cv2.HOUGH_GRADIENT,
+                "dp": 1.0,
+                "minDist": 140,
+                "minRadius": 170,
+                "maxRadius": 230,
+                #  "param1": 140,
+                "param1": 135,
+                "param2": 60,
+            }
+        elif label in ["1e"]:
+            # 1e minDist 40 minRadius 165 maxRadius 190 hcParam1 70 hcParam2 25
+            param = {
+                "method": cv2.HOUGH_GRADIENT,
+                "dp": 1.0,
+                #  "minDist": 200,
+                "minDist": 210,
+                #  "minRadius": 165,
+                "minRadius": 155,
+                "maxRadius": 190,
+                "param1": 70,
+                "param2": 25,
+            }
+        elif label in ["10c"]:
+            # 10c minDist 150 minRadius 140 maxRadius 160 hcParam1 100 hcParam2 41
+            param = {
+                "method": cv2.HOUGH_GRADIENT,
+                "dp": 1.0,
+                "minDist": 150,
+                "minRadius": 140,
+                "maxRadius": 160,
+                #  "param1": 100,
+                "param1": 70,
+                #  "param2": 40,
+                "param2": 30,
+            }
+    elif raw_folder_tag in ["raw5"]:
+        if label in ["1e"]:
+            param = {
+                "method": cv2.HOUGH_GRADIENT,
+                "dp": 1.0,
+                #  "minDist": 200,
+                "minDist": 210,
+                "minRadius": 150,
+                "maxRadius": 160,
+                "param1": 70,
+                "param2": 25,
+            }
+        elif label in ["5c"]:
+            # 5c minDist 150 minRadius 150 maxRadius 170 hcParam1 60 hcParam2 25
+            param = {
+                "method": cv2.HOUGH_GRADIENT,
+                "dp": 1.0,
+                "minDist": 150,
+                "minRadius": 140,
+                "maxRadius": 155,
+                "param1": 60,
+                "param2": 25,
+            }
+        elif label in ["2c"]:
+            # 2c TODO
+            param = {
+                "method": cv2.HOUGH_GRADIENT,
+                "dp": 1.0,
+                "minDist": 200,
+                "minRadius": 125,
+                #  "minRadius": 135,
+                "maxRadius": 160,
+                "param1": 65,
+                "param2": 30,
+            }
     else:
-        # vague default params that should find something
+        # vague default params that might find something, but should not be used
         param = {
             "method": cv2.HOUGH_GRADIENT,
             "dp": 1.0,
-            "minDist": 40,
-            "minRadius": 100,
-            "maxRadius": 230,
+            "minDist": 20,
+            "minRadius": 180,
+            "maxRadius": 240,
             "param1": 75,
             "param2": 40,
         }
@@ -276,18 +367,26 @@ def main():
     #  datafolder = "./mydata"
     datafolder = "."
 
-    raw_folder = "./raw/raw4"
-    raw_folder_full = abspath(join(dir_file, datafolder, raw_folder))
-    print(f"raw_folder_full {raw_folder_full}")
-
     save_folder = "./parsed"
     save_folder_full = abspath(join(dir_file, datafolder, save_folder))
     print(f"save_folder_full {save_folder_full}")
 
     # pad around coin when cropping
-    pad = 20
+    pad = 30
 
-    do_extraction(raw_folder_full, save_folder_full, pad)
+    raw_folder_list = [
+        #  "./raw/raw1",
+        "./raw/raw2",
+        #  "./raw/raw3",
+        #  "./raw/raw4",
+        #  "./raw/raw5",
+    ]
+
+    for raw_folder in raw_folder_list:
+        raw_folder_full = abspath(join(dir_file, datafolder, raw_folder))
+        print(f"raw_folder_full {raw_folder_full}")
+
+        do_extraction(raw_folder_full, save_folder_full, pad)
 
 
 if __name__ == "__main__":
